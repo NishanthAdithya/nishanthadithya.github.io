3d04cc75106f0f942d6d049a26c8b080e0c6984c
NishanthAdithya <144264872+NishanthAdithya@users.noreply.github.com>
2026-01-11 07:17:26 +0000

    Deploying to gh-pages from @ NishanthAdithya/nishanthadithya.github.io@0c60c7cd06d02c9c5f1f73ff53b7b0e7879eaffa 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "NishanthAdithya/nishanth.github.io",
+    "data-repo": "NishanthAdithya/nishanthadithya.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
