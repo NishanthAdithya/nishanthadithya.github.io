12494c5c9807d053d56d11c04075dcc12c35c6c0
NishanthAdithya <144264872+NishanthAdithya@users.noreply.github.com>
2026-01-11 23:44:46 +0000

    Deploying to gh-pages from @ NishanthAdithya/nishanthadithya.github.io@963148aa1f70763a539610f0d800ea65c0b8a70d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-control-and-velocity-kinematics-4dof-openmanipulator-x-robotic-arm",
+          title: 'Control and Velocity Kinematics – 4DoF OpenManipulator-X Robotic Arm',
+          description: "Cartesian Velocity Control, Jacobian-Based IK, and PD Control",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
