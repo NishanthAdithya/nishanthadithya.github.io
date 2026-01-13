324eacc60b6539a8b7939c82e0beb11e79889783
NishanthAdithya <144264872+NishanthAdithya@users.noreply.github.com>
2026-01-13 06:30:03 +0000

    Deploying to gh-pages from @ NishanthAdithya/nishanthadithya.github.io@f35349213a8f75ec56eacd2516b1aa6319d34d31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-experience",
+          title: "Experience",
+          description: "Documentation of my research experience.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/exp/";
+          },
         },{id: "nav-resume",
           title: "Resume",
           description: "",
